ce10922b7e6e002f9d46f8735323a74d243cd820
ksg <kasaga234@gmail.com>
2025-08-19 15:12:02 +0900

    controller 패키지 클래스 생성

--- src/main/java/com/example/spartanewsfeed/comment/controller/CommentController.java
+++ src/main/java/com/example/spartanewsfeed/comment/controller/CommentController.java
@@ -1,4 +1,58 @@
 package com.example.spartanewsfeed.comment.controller;
 
+import com.example.spartanewsfeed.comment.dto.request.RequestDto;
+import com.example.spartanewsfeed.comment.dto.response.ResponseDto;
+import com.example.spartanewsfeed.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/posts/{postId}/comments")
 public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<ResponseDto> createComment(
+            @SessionAttribute(name = "로그인 세션 속성명") Long userId,
+            @PathVariable Long postId,
+            @RequestBody RequestDto requestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentService.createComment(userId, postId, requestDto)); // 201 Create
+        //return ResponseEntity.ok(commentService.createComment(userId, postId, requestDto)); // 200 OK 혹시 몰라 남겨두었습니다.
+    }
+
+    @PatchMapping("/{commentId}") // 댓글 수정
+    public ResponseEntity<ResponseDto> updateComment(
+            @SessionAttribute(name = "로그인 세션 속성명") Long userId,
+            @PathVariable Long postId, // 쓸 곳이 없다.
+            @PathVariable Long commentId,
+            @RequestBody RequestDto requestDto) {
+        return ResponseEntity.ok(commentService.updateComment(userId, commentId, requestDto)); // 200 OK
+    }
+
+    @GetMapping // 댓글 조회
+    public ResponseEntity<List<ResponseDto>> getComments(
+            @PathVariable Long postId){
+        return ResponseEntity.ok(commentService.commentAll(postId)); // 200 OK
+    }
+
+    @GetMapping("/{commentId}")  // 댓글 단건 조회
+    public ResponseEntity<ResponseDto> getCommentById(
+            @PathVariable Long postId, // 쓸 곳이 없다.
+            @PathVariable Long commentId){
+        return ResponseEntity.ok(commentService.findCommentById(commentId)); // 200 OK
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<Void> deleteComment(
+            @SessionAttribute(name = "로그인 세션 속성명") Long userId,
+            @PathVariable Long postId, // 쓸 곳이 없다.
+            @PathVariable Long commentId) {
+        commentService.deleteCommentById(userId, commentId);
+        return ResponseEntity.noContent().build(); // 204 No Content
+    }
 }
