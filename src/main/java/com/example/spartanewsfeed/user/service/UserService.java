f251ddd45ee3152e1ab8372608b1a601e075d047
hanjsnote <enlun@naver.com>
2025-08-21 12:23:54 +0900

    feat: user - 각 필드 변수에 유효성 검증(validation) 적용

--- src/main/java/com/example/spartanewsfeed/user/service/UserService.java
+++ src/main/java/com/example/spartanewsfeed/user/service/UserService.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.server.ResponseStatusException;
 
 
