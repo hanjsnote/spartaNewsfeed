91ac91c2702616a3cf74c48d52e359db4569c3ec
hanjsnote <enlun@naver.com>
2025-08-21 12:21:44 +0900

    feat: user - 각 필드 변수에 유효성 검증(validation) 적용

--- src/main/java/com/example/spartanewsfeed/user/dto/request/UserLoginRequest.java
+++ src/main/java/com/example/spartanewsfeed/user/dto/request/UserLoginRequest.java
@@ -1,14 +1,18 @@
 package com.example.spartanewsfeed.user.dto.request;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserLoginRequest {
 
     @NotBlank(message = "이메일은 필수 입력값입니다.")
-//    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$", message = "유효한 이메일 형식이 아닙니다.") //이메일의 로컬파트+@도메인 부분
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private final String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private final String password;
 
     UserLoginRequest(String email, String password){
--- src/main/java/com/example/spartanewsfeed/user/repository/UserRepository.java
+++ src/main/java/com/example/spartanewsfeed/user/repository/UserRepository.java
@@ -9,16 +9,20 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    //email로 DB에 존재하는지 확인
     boolean existsByEmail(String email);
 
+    //email로 사용자 조회
     Optional<User> findByEmail(String email);
 
+    //email로 사용자 조회 없으면 404 에러
     default  User findByEmailOrElseThrow(String email){
         return findByEmail(email).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
         );
     }
 
+    //id로 사용자 조회 없으면 404 에러
     default User findByIdOrElseThrow(Long id){
         return findById(id).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
