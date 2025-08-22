038c3ab15cedcf0f4dbe09cc288d590a609bc541
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-08-22 11:34:36 +0900

    feat: post - follow 연관관계 구현 및 url주소 수정

--- src/main/java/com/example/spartanewsfeed/post/repository/PostRepository.java
+++ src/main/java/com/example/spartanewsfeed/post/repository/PostRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAllByUserId(Long userId, Pageable pageable);
 
     void deleteByUserIdAndId(Long userId, Long id);
 
+    Page<Post> findAllByUserIdIn(List<Long> userIds, Pageable pageable);
 }
