c77db68b471f7cacaa7f2a17e1422b31dc671625
ksg <kasaga234@gmail.com>
2025-08-22 15:24:10 +0900

    comment 사소한 코드 정리

--- src/main/java/com/example/spartanewsfeed/comment/entity/Comment.java
+++ src/main/java/com/example/spartanewsfeed/comment/entity/Comment.java
@@ -10,11 +10,12 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
-@Table(name = "comment")
+@Table(name = "comments")
 public class Comment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/example/spartanewsfeed/comment/repository/CommentRepository.java
+++ src/main/java/com/example/spartanewsfeed/comment/repository/CommentRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
     List<Comment> findByPost_Id(Long postId, Pageable pageable);
-
     Page<Comment> findByPost(Post post, Pageable pageable);
 }
