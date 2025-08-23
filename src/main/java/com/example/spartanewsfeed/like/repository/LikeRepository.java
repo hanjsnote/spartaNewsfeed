a770118241c14dac5a20315d2e606d6345c5be0e
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-23 21:44:01 +0900

    fix: like 삭제 기능 수정

--- src/main/java/com/example/spartanewsfeed/like/repository/LikeRepository.java
+++ src/main/java/com/example/spartanewsfeed/like/repository/LikeRepository.java
@@ -5,10 +5,11 @@ import com.example.spartanewsfeed.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     int countByPost(Post post);
     List<Like> findAllByPostId(Long postId);
     boolean existsByPostIdAndUserId(Long postId, Long userId);
-    void deleteByPostIdAndUserId(Long postId, Long userId);
+    Like findByPostIdAndUserId(Long postId, Long userId);
 }
